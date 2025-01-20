fbde6b442ab4b45428948f82cf815641040d1b45
mayshavit <mayshavit1234@gmail.com>
2025-01-20 15:02:09 +0200

    Update ec2.tf

--- .github/terraform/aws/ec2.tf
+++ .github/terraform/aws/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user data testhgjsdsd
+  # ec2 have plain text secrets in user data testhgjsdsdhgh
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
